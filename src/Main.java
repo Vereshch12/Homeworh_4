60198439b57faf177b877bcebce80d4df208c62a
Vereshchagin_Mikhail <farasers@mail.ru>
2022-08-11 18:45:15 +0300

    Задание №2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         // Задание №1
         short i = 1;
         while (i <= 10){
@@ -10,6 +11,15 @@ public class Main {
         i--;
         for (; i >= 1; i--){
             System.out.print(i + " ");
+        }
+
+
+        //Задание №2
+        System.out.println("\n");
+        int dayOfMonth = 5;
+        for (; dayOfMonth <= 31; dayOfMonth++){
+            if ((dayOfMonth - 5) % 7 == 0)
+                System.out.println("Сегодня пятница, " + dayOfMonth + "-е число. Необходимо подготовить отчет.");
         }
     }
 }
