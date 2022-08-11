0c0f568dc30e4457de1792d2ce812cb77d002cf0
Vereshchagin_Mikhail <farasers@mail.ru>
2022-08-11 18:33:57 +0300

    ДЗ №4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задание №1
+        short i = 1;
+        while (i <= 10){
+            System.out.print(i + " ");
+            i += 1;
+        }
+        System.out.println();
+        i--;
+        for (; i >= 1; i--){
+            System.out.print(i + " ");
+        }
     }
 }
