53b24b9618d5b412c830d7171f699ac3e3ef0871
Vereshchagin_Mikhail <farasers@mail.ru>
2022-08-12 18:57:10 +0300

    Все исправил)

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,22 @@ public class Main {
             i += 1;
         }
         System.out.println();
-        i--;
-        for (; i >= 1; i--){
+        for (i = 10; i >= 1; i--){
             System.out.print(i + " ");
         }
 
 
         //Задание №2
         System.out.println("\n");
-        int dayOfMonth = 5;
-        for (; dayOfMonth <= 31; dayOfMonth++){
+        for (int dayOfMonth = 5; dayOfMonth <= 31; dayOfMonth++){
             if ((dayOfMonth - 5) % 7 == 0)
                 System.out.println("Сегодня пятница, " + dayOfMonth + "-е число. Необходимо подготовить отчет.");
         }
 
         //Задание №3
         System.out.println();
-        int year = 0;
         int todaysYear = 2022;
-        for (;year <= todaysYear + 100; year++){
+        for (int year = 0; year <= todaysYear + 100; year++){
             if (year % 79 == 0){
                 if (todaysYear - year <= 200)
                     System.out.println(year);
